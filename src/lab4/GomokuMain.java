261679f5c0168ea90bbb2fc6aa40516e7a1cf847
Sebastian <sebbe1991+git@gmail.com>
2011-02-16 15:25:32 +0100

    It now runs.

---
+++ src/lab4/GomokuMain.java
@@ -0,0 +1,17 @@
+package lab4;
+
+import lab4.data.GomokuGameState;
+import lab4.client.GomokuClient;
+
+public class GomokuMain {
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Usage: java GomokuMain <portnumber>");
+			return;
+		}
+
+		int port = Integer.parseInt(args[0]);
+		GomokuClient client = new GomokuClient(port);
+		GomokuGameState gameState = new GomokuGameState(client);
+	}
+}
